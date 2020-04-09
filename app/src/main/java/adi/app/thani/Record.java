c51f87db6b525cb1a308190063eef7062185fc0c
adithya <incredibleatha2@gmail.com>
2020-04-09 13:58:49 -0700

    edit
    Signed-off-by: adithya <incredibleatha2@gmail.com>

--- app/src/main/java/adi/app/thani/Record.java
+++ app/src/main/java/adi/app/thani/Record.java
@@ -3,22 +3,14 @@ package adi.app.thani;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Picture;
-import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaRecorder;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Environment;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -26,20 +18,13 @@ import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import org.jcodec.common.*;
+
 import org.jcodec.api.android.AndroidSequenceEncoder;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import static android.os.Environment.getExternalStorageDirectory;
-import static android.os.Environment.getExternalStorageState;
 
 public class Record extends Activity {
     int permissionsgranted;
